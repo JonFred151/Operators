b648f563a2921fa75a3ffb7bfa03988c8e221cec
Jon Frederick <jonFred151@gmail.com>
2023-11-27 10:43:18 -0600

    PascalCasing

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -13,20 +13,20 @@ namespace Operators
                 int b = 2;
             double radius;
                 // calls the method to add a+b
-                int total = sum(a, b);
-                Console.WriteLine(total);
+                int Total = Sum(a, b);
+                Console.WriteLine(Total);
                 //calls the method to subtract a-b
-                int sub = subtract(a, b); 
-                Console.WriteLine(sub);
+                int Sub = Subtract(a, b); 
+                Console.WriteLine(Sub);
                 //calls the method to multiple a*b
-                int product = multi(a, b);
-                Console.WriteLine(product);
+                int Product = Multi(a, b);
+                Console.WriteLine(Product);
                 //callsw the method to divide 
-                int divisor = divide(a, b); 
-                Console.WriteLine(divisor);
+                int Divisor = Divide(a, b); 
+                Console.WriteLine(Divisor);
                //method to call the remainder a%b
-               int remain = remainder(a, b);
-              Console.WriteLine(remain);
+               int Remain = Remainder(a, b);
+              Console.WriteLine(Remain);
 
             Console.WriteLine("Enter a radius");
              radius = double.Parse(Console.ReadLine());
@@ -38,23 +38,23 @@ namespace Operators
             
         }
 
-           public static int sum(int a, int b)
+           public static int Sum(int a, int b)
         {
             return a + b;
         } 
-            public static int subtract(int a, int b)
+            public static int Subtract(int a, int b)
         {
             return a - b;
         }
-            public static int multi(int a, int b)
+            public static int Multi(int a, int b)
         {
             return a * b;
         }
-            public static int divide(int a, int b)
+            public static int Divide(int a, int b)
         {
             return a/b;
         }
-            public static int remainder(int a, int b)
+            public static int Remainder(int a, int b)
         {
             return (a % b);
         }
